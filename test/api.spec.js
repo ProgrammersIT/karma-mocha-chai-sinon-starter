5ebb9a0d91b57a3482c57a51cf9bdfb9dc1735e2
Pedro Barros <pedrobarrostech@gmail.com>
2017-03-15 15:55:03 -0300

    Improvements

--- test/api.spec.js
+++ test/api.spec.js
@@ -5,17 +5,70 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Users', () => {
-  describe('/GET user', () => {
-	  it('it should GET all the users', (done) => {
-		chai.request('http://localhost:8080')
-		    .get('/users')
+describe('Books', () => {
+	/*describe('/GET book', () => {
+	  it('it should GET all the books', (done) => {
+		chai.request('http://localhost:3000')
+		    .get('/books')
 		    .end((err, res) => {
 			  	res.should.have.status(200);
 			  	res.body.should.be.a('array');
-			  	res.body.length.should.be.eql(0);
+			  	res.body.length.should.be.eql(4);
 		      done();
 		    }); 
 	  });
-  });
+  });*/
+
+	describe('/POST book', () => {
+		it('it should POST a book', (done) => {
+			let book = {
+				title: "The Lord of the Rings 2",
+				author: "J.R.R. Tolkien",
+				year: 1954,
+				pages: 1170
+			}
+			chai.request('http://localhost:3000')
+				.post('/books')
+				.send(book)
+				.end((err, res) => {
+					res.should.have.status(201);
+					res.body.should.have.property('title');
+					res.body.should.have.property('author');
+					res.body.should.have.property('pages');
+					res.body.should.have.property('year');
+					done();
+				});
+		});
+
+
+		describe('/GET book', () => {
+			it('it should GET a book by given id', (done) => {
+				chai.request('http://localhost:3000')
+					.get('/books/1')
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('title');
+			  		res.body.should.have.property('author');
+			  		res.body.should.have.property('pages');
+			  		res.body.should.have.property('year');
+						res.body.should.have.property('id').eql(1);
+						done();
+					});
+			});
+		});
+
+
+		describe('/DELETE book', () => {
+			it('it should DELETE a book by given id', (done) => {
+				chai.request('http://localhost:3000')
+					.get('/books/1')
+					.end((err, res) => {
+						res.should.have.status(200);
+						done();
+					});
+			});
+		});
+
+	});
 });
